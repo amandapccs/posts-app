86addc714dcb7845f0d68c38c14518ed70e902c1
Amanda Soares <amandchen@hotmail.com>
2022-10-30 18:57:32 -0300

    App: adiciona componente app e estilização

---
+++ src/pages/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const EditDiv = styled.div`
+    width: 100%;
+    max-width: 576px;
+    background-color: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+    margin: auto;
+`;
